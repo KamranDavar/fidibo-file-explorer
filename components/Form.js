3935d241504c7f12eb29ea3a204d3a623a4dccd2
kamran <kamran.davar.kd@gmail.com>
2023-02-28 17:08:46 +0330

    input fucus

--- components/Form.js
+++ components/Form.js
@@ -5,13 +5,18 @@ import { useAddFiles } from "../logics/hooks/useAddFiles";
 const Form = ({ mode,name,setShowForm,fileId, isDirectory }) => {
   const createFile= useAddFiles()
 
-  const { register, handleSubmit, watch, formState } = useForm();
+  const { register, handleSubmit, watch, setFocus } = useForm();
   const onSubmit = (data) =>{ console.log(data)
     data.isDirectory=isDirectory
     data.fileId=fileId
     createFile.mutate(data)
     setShowForm(false)
 };
+
+React.useEffect(() => {
+  setFocus("name");
+}, [setFocus]);
+
 
   console.log(watch("example")); // watch input value by passing the name of it
 
